e52363db72c546bf6cbb329c62808b93c39afaea
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2021-09-19 11:38:07 +0900

    Update JavaEnumStudy.java
    EnumSet のサンプルを追加。

--- src/JavaEnumStudy.java
+++ src/JavaEnumStudy.java
@@ -1,3 +1,5 @@
+import java.util.EnumSet;
+
 import enums.Statuses;
 
 /**
@@ -18,6 +20,27 @@ public class JavaEnumStudy {
         for (Statuses status : Statuses.values()) {
             System.out.println("status.name() : " + status.name() + ", status.ordinal() : " + status.ordinal()
                     + ", status.getCode() : " + status.getDescription());
+        }
+
+        System.out.println("----------");
+
+        // ビットフラグには EnumSet を使用する。
+        EnumSet<Statuses> statusSets;
+
+        statusSets = EnumSet.allOf(Statuses.class);
+
+        if (statusSets.contains(Statuses.OK)) {
+            System.out.println("EnumSet.allOf(Statuses.class) : OKあり");
+        } else {
+            System.out.println("EnumSet.allOf(Statuses.class) : OKなし");
+        }
+
+        statusSets = EnumSet.of(Statuses.NG);
+
+        if (statusSets.contains(Statuses.OK)) {
+            System.out.println("EnumSet.of(Statuses.NG) : OKあり");
+        } else {
+            System.out.println("EnumSet.of(Statuses.NG) : OKなし");
         }
     }
 }
